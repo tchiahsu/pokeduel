3c4951947a0cb20f8fe1108f071b28fbe7001da3
Tony Hsu <hsutai.c@gmail.com>
2025-07-06 17:45:55 -0400

    first version for controller and input reader

---
+++ src/build/InputReader.ts
@@ -0,0 +1,40 @@
+import process from 'process';
+import * as readline from 'readline';
+
+export default class InputReader {
+    private rl: readline.Interface;
+
+    constructor() {
+        this.rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
+    }
+
+    private prompt(question: string): Promise<string> {
+        return new Promise((resolve) => {
+            this.rl.question(question, (answer) => {
+                resolve(answer.trim());
+            });
+        });
+    }
+
+    async getPlayerName(): Promise<string> {
+        const name = await this.prompt('Enter your player name: ');
+        return name;
+    }
+
+    async getPlayerTeamChoice(): Promise<string[]> {
+        const teamString = await this.prompt('Choose your team and Pokemon names (separated by comas)')
+        return teamString.split(',').map(name => name.trim());
+    }
+
+    async getMove(): Promise<string> {
+        const move = await this.prompt('Enter your move (or switch choice): ');
+        return move;
+    }
+
+    close(): void {
+        this.rl.close();
+    }
+}
