b982ebf17632585214f406684419f6ed62ffacba
Bhoomika <bhoomig252@gmail.com>
2025-07-30 18:16:52 -0400

    modified search pokemon bar and added a search button

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -10,7 +10,7 @@ export default function SearchBar({ value, onChange }: Props) {
             placeholder="Search Pokémon..."
             value={value}
             onChange={(e) => onChange(e.target.value)}
-            className="w-full px-4 py-2 border border-gray-400 rounded-lg shadow-sm focus:blue-400"
+            className="w-full h-12 px-4 py-2 border-2 border-gray-400 rounded-lg shadow-sm focus:outline-none focus:border-red-500"
         />
     );
 }
