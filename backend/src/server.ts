321787aca6817bd2a88f46dc94ca23a77b9a35fe
Harrison Pham <hpham1997@gmail.com>
2025-07-30 22:46:55 -0400

    create endpoints for room ID requests and created server socket listeners for client connections

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
+import cors from "cors";
 import PokemonApiFetcher from "./model/PokemonApiFetcher.js";
 import BattleModel from "./model/BattleModel.js";
+import RoomManager from "./services/RoomManager.js";
+import ShortUniqueId from "short-unique-id";
 
 const app = express();
 const server = http.createServer(app);
@@ -10,30 +13,34 @@ const io = new Server(server, { cors: { origin: "*" } });
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
+app.use(cors());
 
-app.get("/get-default-pokemon", async (req, res) => {
+app.get("/pokemon/default", async (req, res) => {
   const defaultPokemon = await PokemonApiFetcher.getDefaultPokemon();
   res.json(defaultPokemon);
 });
 
-app.get("/get-pokemon-moves/:name", async (req, res) => {
+app.get("/pokemon/:name/moves", async (req, res) => {
   const pokemonMoves = await PokemonApiFetcher.getPokemonMoves(req.params.name);
   res.json(pokemonMoves);
 });
 
 // Get the stats of the selected pokemon
-app.get("/get-pokemon-data/:name", async (req, res) => {
-  const { name } = req.params;
+app.get("/pokemon/:name/stats", async (req, res) => {
+  const pokemonName = req.params.name;
 
-  const pokemonData = await PokemonApiFetcher.createOnePokemonStats(name);
+  const pokemonData = await PokemonApiFetcher.createOnePokemonData(
+    pokemonName,
+    []
+  );
   res.json(pokemonData);
 });
 
 // Get the moves of the selected pokemon
-app.get("/get-move-data/:move", async (req, res) => {
-  const { move } = req.params;
+app.get("/moves/:name", async (req, res) => {
+  const moveName = req.params.name;
 
-  const moveData = await PokemonApiFetcher.createOneMoveData(move);
+  const moveData = await PokemonApiFetcher.createOneMoveData(moveName);
   res.json(moveData);
 });
 
@@ -41,67 +48,92 @@ server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}/`);
 });
 
-(async () => {
-  const model = new BattleModel();
-  await model.setPlayer("1234", "harrison", {
-    charizard: ["flamethrower", "air-slash", "dragon-claw", "earthquake"],
-    blastoise: ["hydro-pump", "ice-beam", "surf", "bite"],
+const roomManager: RoomManager = new RoomManager();
+
+io.on("connection", (socket) => {
+  console.log(`Player ${socket.id} has connected`);
+
+  socket.on("joinRoom", (roomID) => {
+    socket.join(roomID);
+    if (!roomManager.isRoom(roomID)) {
+      roomManager.createRoom(roomID);
+    }
+    roomManager.addPlayerToRoom(socket.id, roomID);
+    socket.emit("joinRoom", { message: `Joined room ${roomID}` });
   });
 
-  console.log(`player 1 ${model.getPlayer1ID()}`);
-  console.log(model.getPlayers());
-
-  await model.setPlayer("5678", "pham", {
-    alakazam: ["psychic", "shadow-ball", "recover", "calm-mind"],
-    machamp: ["dynamic-punch", "earthquake", "stone-edge", "bulk-up"],
+  socket.on("setPlayer", async (data) => {
+    const roomID: string = roomManager.getPlayerRoom(socket.id);
+    const battleModel: BattleModel = roomManager.getBattleModel(roomID);
+    await battleModel.setPlayer(socket.id, data.name, data.teamSelection);
+    if (battleModel.hasTwoPlayers()) {
+      io.to(roomID).emit("gameStart", () => {
+        console.log(`Room ${roomID}'s battle has begun`);
+      });
+    }
   });
 
-  console.log(model.getPlayers());
+  socket.on("submitMove", (data) => {
+    const roomID: string = roomManager.getPlayerRoom(socket.id);
+    const battleModel: BattleModel = roomManager.getBattleModel(roomID);
+    battleModel.addMove(socket.id, data);
+    if (battleModel.readyToHandleTurn()) {
+      battleModel.handleTurn();
+      const turnSummary = battleModel.getTurnSummary();
+      const player1ID = battleModel.getPlayer1ID();
+      const player2ID = battleModel.getPlayer2ID();
+      io.to(player1ID).emit("turnSummary", turnSummary[player1ID]);
+      io.to(player2ID).emit("turnSummary", turnSummary[player2ID]);
+    }
+  });
 
-  model.addMove("1234", { action: "switch", index: 1 });
-  model.addMove("5678", { action: "switch", index: 1 });
+  socket.on("disconnect", () => {
+    const roomID: string = roomManager.getPlayerRoom(socket.id);
+    const battleModel: BattleModel = roomManager.getBattleModel(roomID);
+    roomManager.removePlayerFromRoom(socket.id);
+    if (battleModel && battleModel.hasTwoPlayers()) {
+      const remainingPlayer =
+        socket.id === battleModel.getPlayer1ID()
+          ? battleModel.getPlayer2ID()
+          : battleModel.getPlayer1ID();
+      io.to(remainingPlayer).emit("endGame", {
+        message: `Opponent has disconnected`,
+      });
+    } else if (roomManager.isWaitingRoomEmpty(roomID)) {
+      roomManager.deleteRoom(roomID);
+    }
+  });
+});
 
-  console.log(model.getMoves());
-  if (model.readyToHandleTurn()) {
-    model.handleTurn();
-    console.log(model.getMessages());
+app.post("/room", (req, res) => {
+  const { randomUUID } = new ShortUniqueId({ length: 10 });
+  let roomID: string = randomUUID();
+  while (roomManager.isRoom(roomID)) {
+    roomID = randomUUID();
   }
-})();
+  roomManager.createRoom(roomID);
+  res.status(200).json({ id: roomID });
+});
 
-// model.addMove("1234", { action: "switch", index: 2 });
-// model.addMove("5678", { action: "switch", index: 2 });
+app.delete("/room/:id", (req, res) => {
+  const roomID = req.params.id;
+  roomManager.deleteRoom(roomID);
+  res.status(200).json({ message: `Room ${roomID} deleted` });
+});
 
-// if (model.readyToHandleTurn()) {
-//   model.handleTurn();
-//   console.log(model.getMessages());
-// }
+app.get("/room/:id", (req, res) => {
+  const roomID = req.params.id;
+  if (!roomManager.isRoom(roomID)) {
+    return res
+      .status(404)
+      .json({ available: false, message: `Room ${roomID} not found` });
+  } else if (roomManager.IsRoomFull(roomID)) {
+    return res
+      .status(409)
+      .json({ available: false, message: `Room ${roomID} is full` });
+  }
 
-// io.on("connection", async (socket) => {
-//   console.log(`Player ${socket.id} has connected`);
-
-//   await model.setPlayer(socket.id, "harrison", {
-//     charizard: ["flamethrower", "air-slash", "dragon-claw", "earthquake"],
-//     blastoise: ["hydro-pump", "ice-beam", "surf", "bite"],
-//   });
-
-//   await model.setPlayer(socket.id, "pham", {
-//     alakazam: ["psychic", "shadow ball", "recover", "calm mind"],
-//     machamp: ["dynamic punch", "earthquake", "stone edge", "bulk up"],
-//   });
-
-//   socket.on("setPlayer", async (data) => {
-//     await model.setPlayer(socket.id, data.name, data.teamSelection);
-//     console.log(model.checkSetStatus());
-//   });
-
-//   socket.on("submitMove", (data) => {
-//     model.addMove(socket.id, data);
-
-//     if (model.readyToHandleTurn()) {
-//       model.handleTurn();
-//       model.getMessages();
-//     }
-//   });
-
-//   socket.emit("update-game", { message: "hello" });
-// });
+  return res
+    .status(200)
+    .json({ available: true, message: `Room ${roomID} found` });
+});
