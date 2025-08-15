bf2d498d2f82e94e059a12d726fad7d1cef6d52f
Bhoomika <bhoomig252@gmail.com>
2025-08-15 09:55:51 -0400

    added new bg and pixelated poke ball

--- frontend/src/components/ActivePokeCount.tsx
+++ frontend/src/components/ActivePokeCount.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import pokeBall from '../assets/pokeball3.png';
+import pokeBall from '../assets/poke_pixel.png';
 
 interface Pokemon {
   name: string;
@@ -14,7 +14,7 @@ interface ActivePokeProps {
 
 const ActivePokeCount: React.FC<ActivePokeProps> = ({ team }) => {
   return (
-    <div className="flex gap-2 p-2">
+    <div className="flex gap-2 p-2 mt-1 mb-1">
       {team.map((poke, index) => {
         //Logic to be changed
         const isFainted = poke.hp <= 0 || poke.name === '';
@@ -23,7 +23,7 @@ const ActivePokeCount: React.FC<ActivePokeProps> = ({ team }) => {
             key={index}
             src={pokeBall}
             alt="PokéBall"
-            className="w-8 h-8 select-none pointer-events-none"
+            className="w-10 h-10 select-none pointer-events-none"
             style={{ opacity: isFainted ? 0.3 : 1 }}
           />
         );
