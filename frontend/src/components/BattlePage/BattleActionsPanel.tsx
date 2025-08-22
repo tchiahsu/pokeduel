51f7c8dfa1a4fc5e1b012e47ed0f928689e4fd1e
Bhoomika <bhoomig252@gmail.com>
2025-08-22 18:39:52 -0400

    hover effect for action buttons

--- frontend/src/components/BattlePage/BattleActionsPanel.tsx
+++ frontend/src/components/BattlePage/BattleActionsPanel.tsx
@@ -12,24 +12,45 @@ interface BattleActionsPanelProps {
 const BattleActionsPanel: React.FC<BattleActionsPanelProps> = ({ onSelect, onQuit }) => {
   return (
     <div className="flex flex-col flex-1 h-full p-4 rounded-lg bg-gray-300/80 justify-center items-center gap-4">
-      <button
-        onClick={() => onSelect('attack')}
-        className="w-13 h-13 bg-[#FFA500]/80 hover:bg-gray-500/80 rounded-full flex items-center justify-center"
-      >
-        <GiMineExplosion className="text-white text-3xl" />
-      </button>
-      <button
-        onClick={() => onSelect('switch')}
-        className="w-13 h-13 bg-[#3B4CCA]/80 hover:bg-gray-500/80  rounded-full flex items-center justify-center"
-      >
-        <RiExchange2Line className="text-white text-3xl" />
-      </button>
-      <button
-        onClick={onQuit}
-        className="w-13 h-13 bg-[#FF0000]/80 hover:bg-gray-500/80 rounded-full flex items-center justify-center"
-      >
-        <IoMdExit className="text-white text-3xl" />
-      </button>
+      
+      {/* Attack Button */}
+      <div className="relative group">
+        <div className="absolute -top-8 left-1/2 -translate-x-1/2 px-2 py-1 text-xs rounded bg-white text-black opacity-0 group-hover:opacity-100 transition-opacity">
+          ATTACK
+        </div>
+        <button
+          onClick={() => onSelect('attack')}
+          className="w-13 h-13 bg-[#FFA500]/80 hover:bg-gray-500/80 rounded-full flex items-center justify-center"
+        >
+          <GiMineExplosion className="text-white text-3xl" />
+        </button>
+      </div>
+
+      {/* Switch Button */}
+      <div className="relative group">
+        <div className="absolute -top-8 left-1/2 -translate-x-1/2 px-2 py-1 text-xs rounded bg-white text-black opacity-0 group-hover:opacity-100 transition-opacity">
+          SWITCH
+        </div>
+        <button
+          onClick={() => onSelect('switch')}
+          className="w-13 h-13 bg-[#3B4CCA]/80 hover:bg-gray-500/80 rounded-full flex items-center justify-center"
+        >
+          <RiExchange2Line className="text-white text-3xl" />
+        </button>
+      </div>
+
+      {/* Run Button */}
+      <div className="relative group">
+        <div className="absolute -top-8 left-1/2 -translate-x-1/2 px-2 py-1 text-xs rounded bg-white text-black opacity-0 group-hover:opacity-100 transition-opacity">
+          RUN
+        </div>
+        <button
+          onClick={onQuit}
+          className="w-13 h-13 bg-[#FF0000]/80 hover:bg-gray-500/80 rounded-full flex items-center justify-center"
+        >
+          <IoMdExit className="text-white text-3xl" />
+        </button>
+      </div>
     </div>
   );
 };
