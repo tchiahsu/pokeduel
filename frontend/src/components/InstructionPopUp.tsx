beb8787399ca293afe66a18cd6c356529113c830
Bhoomika <bhoomig252@gmail.com>
2025-09-05 19:42:51 -0400

    selection instruction pop-up

--- frontend/src/components/InstructionPopUp.tsx
+++ frontend/src/components/InstructionPopUp.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { IoClose } from 'react-icons/io5';
 import Button from './Button';
+import SelectionInstruction from './TeamSelection/SelectionInstruction';
 
 interface InstructionsPopupProps {
   onClose: () => void;
@@ -11,21 +12,21 @@ const InstructionsPopup: React.FC<InstructionsPopupProps> = ({ onClose }) => {
 
   return (
     <div className="fixed inset-0 bg-black/50 z-50 flex items-center justify-center backdrop-blur-sm">
-      <div className="relative bg-gray-300/80 rounded-xl p-8 shadow-xl w-12/13 max-w-2xl h-5/8 text-gray-700">
+      <div className="relative flex flex-col bg-gray-300/80 rounded-xl p-8 shadow-xl w-12/13 max-w-3xl h-4/5 text-gray-700">
 
         {/* Close Button */}
         <button
           onClick={onClose}
-          className="absolute top-2 right-2 text-2xl text-gray-700 hover:text-black"
+          className="absolute top-4 right-4 text-3xl text-gray-600 hover:text-gray-700"
         >
           <IoClose />
         </button>
 
         {/* Title */}
-        <h2 className="text-2xl font-bold mb-4 text-center text-[#2563eb]">How to...</h2>
+        <h2 className="text-2xl h-15 font-bold mb-4 text-center text-[#2563eb]">How to...</h2>
 
         {/* Tab Buttons */}
-        <div className="flex justify-center gap-4 mb-6 flex-wrap">
+        <div className="flex h-15 justify-center gap-4 mb-6 flex-wrap">
           <Button
             onClick={() => setActiveTab('start')}
             size="sm"
@@ -50,7 +51,7 @@ const InstructionsPopup: React.FC<InstructionsPopupProps> = ({ onClose }) => {
         </div>
 
         {/* Tab Content */}
-        <div className="text-left space-y-4 px-2 sm:px-6 text-[#2563eb]">
+        <div className="flex flex-1 overflow-y-auto px-1 pr-4 scroll-smooth space-y-4 text-[#2563eb]">
           {activeTab === 'start' && (
             <div className='flex flex-col gap-5'>
               <h3 className="text-lg">How to Start the Game</h3>
@@ -58,21 +59,16 @@ const InstructionsPopup: React.FC<InstructionsPopupProps> = ({ onClose }) => {
                 <li>Click on <b className='bg-gray-300 p-1 rounded-lg'>Solo Battle</b> to play with Computer or click on <b className='bg-gray-300 p-1 rounded-lg'>Multiplayer</b> to battle a friend</li>
                 <li>In Multiplayer, create or join a Room using BattleRoomID</li>
                 <li>After joining, click <b className='bg-amber-400 p-1 text-gray-100 rounded-lg'>START GAME</b> to proceed to Team Selection</li>
+                <li>After joining, click <b className='bg-amber-400 p-1 text-gray-100 rounded-lg'>START GAME</b> to proceed to Team Selection</li>
+                <li>After joining, click <b className='bg-amber-400 p-1 text-gray-100 rounded-lg'>START GAME</b> to proceed to Team Selection</li>
+                <li>After joining, click <b className='bg-amber-400 p-1 text-gray-100 rounded-lg'>START GAME</b> to proceed to Team Selection</li>
+                <li>After joining, click <b className='bg-amber-400 p-1 text-gray-100 rounded-lg'>START GAME</b> to proceed to Team Selection</li>
+                <li>After joining, click <b className='bg-amber-400 p-1 text-gray-100 rounded-lg'>START GAME</b> to proceed to Team Selection</li>
               </ol>
             </div>
           )}
 
-          {activeTab === 'team' && (
-            <div className='flex flex-col gap-5'>
-              <h3 className="text-lg font-semibold">How to Select Team</h3>
-              <ol className="flex flex-col gap-5 list-decimal list-inside text-gray-700">
-                <li>Select a maximum of 6 Pokemon for your team</li>
-                <li>Click on a Pokemon to view its stats and moves</li>
-                <li>Add the moves and remember each Pokemon can have up to 4 moves</li>
-                <li>Once your team is ready, click <b className='bg-amber-400 p-1 text-gray-100 rounded-lg'>START</b></li>
-              </ol>
-            </div>
-          )}
+          {activeTab === 'team' && <SelectionInstruction />}
 
           {activeTab === 'battle' && (
             <div className='flex flex-col gap-5'>
