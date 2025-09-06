dab9dce78769fccdfbaae6c9c4aa2c71747545b0
Tony Hsu <hsutai.c@gmail.com>
2025-09-06 16:14:37 -0400

    fixed spacing in display panel

--- frontend/src/components/BattlePage/BattleDisplayPanel.tsx
+++ frontend/src/components/BattlePage/BattleDisplayPanel.tsx
@@ -125,15 +125,31 @@ const BattleDisplayPanel: React.FC<BattleDisplayPanelProps> = ({
             }
             onSwitchSelect?.(i);
           }}
-          className={`flex w-full justify-between gap-5 p-3 rounded-md flex-row hover:scale-102 active:scale-98 shadow-lg overflow-x-hidden
-            poke.hp <= 0 ? "bg-red-300 text-gray-500" : "bg-green-100"
+          className={`flex w-full gap-5 p-3 rounded-md flex-row hover:scale-102 active:scale-98 shadow-lg overflow-x-hidden
+            ${poke.hp <= 0 ? "bg-red-300 text-gray-500" : "bg-green-100"}
           }`}
         >
-          {/* Button Content */}
-          <div className="flex flex-1">
-            {poke.frontSprite && <img src={poke.frontSprite} alt={poke.name} className="h-full w-auto max-w-10" />}
+          {/* Pokemon Image */}
+          {poke.frontSprite && <img src={poke.frontSprite} alt={poke.name} className="h-full w-auto max-w-10" />}
 
-            <div className="flex flex-col justify-between pl-3">
+          {/* Right Side */}
+          <div className="flex flex-col w-full">
+            {/* Pokemon Name */}
+            <div className="flex justify-start overflow-hidden whitespace-nowrap">
+              {toTitleCase(removeHyphen(poke.name))}
+            </div>
+
+            {/* Pokemon Type and HP */}
+            <div className="flex flex-row justify-between">
+              {/* Type */}
+              <div className="flex">{poke.types.length === 1 ? poke.types[0] : poke.types[0] + ", " + poke.types[1]}</div>
+
+              {/* HP */}
+              <div className="flex">{poke.hp}/{poke.maxHP}</div>
+            </div>
+          </div>
+
+            {/* <div className="flex flex-col justify-between pl-3">
               <span className="flex font-bold select-none overflow-hidden whitespace-nowrap">
                 {toTitleCase(removeHyphen(poke.name))}
               </span>
@@ -144,11 +160,13 @@ const BattleDisplayPanel: React.FC<BattleDisplayPanelProps> = ({
               </div>
             </div>
           </div>
+
           <div className="flex">
             <div className="flex text-xs items-baseline-last">
               {poke.hp}/{poke.maxHP}
             </div>
-          </div>
+          </div> */}
+
         </button>
       ))}
     </div>
